52a936243f6cab5c5cbf014b3ff5df284fae119a
musitec_aqp <mym.musitecperu@gmail.com>
2026-05-25 22:35:06 -0500

    update frontend

--- src/components/showOptions.jsx
+++ src/components/showOptions.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 import "./showOptions.css"
 
 function ShowOptions({
@@ -9,14 +9,33 @@ function ShowOptions({
     onChangeOption,
     text
 }) {
+    const optionsRef=useRef(null)
     const [open, setOpen] = useState(false)
     const handleSelect = (value) => {
         onChangeOption(value)
         setOpen(false)
     }
+    const [isDragging, setIsDragging] = useState(false)
+    const [startY, setStartY] = useState(0)
+    const [scrollTop, setScrollTop] = useState(0)
+    const handleMouseDown = (e) => {
+        setIsDragging(true)
+        setStartY(e.pageY - optionsRef.current.offsetTop)
+        setScrollTop(optionsRef.current.scrollTop)
+    }
+    const handleMouseMove = (e) => {
+        if(!isDragging) return
+        e.preventDefault()
+        const y = e.pageY - optionsRef.current.offsetTop
+        const walk = (y - startY) * 1.5
+        optionsRef.current.scrollTop = scrollTop - walk
+    }
+    const handleMouseUp = () => {
+        setIsDragging(false)
+    }
     return (
         <div className="bar-container">
-            <h2>{title} {open?option:""}:</h2>
+            <div><strong style={{"fontSize":"24px"}}>{title}</strong>/<p style={{"fontSize":"24px"}}>{open?"":values.indexOf(option)}</p></div>
             <div className="custom-select">
                 <div
                     className="selected-box"
@@ -28,7 +47,14 @@ function ShowOptions({
                             : "Selecciona una opción"
                     }
                 </div>
-                <div className={`options-container ${open ? "open" : ""}`}>
+                <div 
+                    ref={optionsRef}
+                    className={`options-container ${open ? "open" : ""}`}
+                    onMouseDown={handleMouseDown}
+                    onMouseMove={handleMouseMove}
+                    onMouseLeave={handleMouseUp}
+                    onMouseUp={handleMouseUp}
+                >
                     {
                         labels.map((label, index) => {
                             const value = values[index]
