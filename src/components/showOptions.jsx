b6ce4d57de498c62aae32fdd08124c37727b792d
musitec_aqp <mym.musitecperu@gmail.com>
2026-05-25 22:21:13 -0500

    update frontend

--- src/components/showOptions.jsx
+++ src/components/showOptions.jsx
@@ -1,27 +1,50 @@
+import { useState } from "react"
 import "./showOptions.css"
 
-function ShowOptions({ title, labels, values, option, onChangeOption, text }) {
+function ShowOptions({
+    title,
+    labels,
+    values,
+    option,
+    onChangeOption,
+    text
+}) {
+    const [open, setOpen] = useState(false)
+    const handleSelect = (value) => {
+        onChangeOption(value)
+        setOpen(false)
+    }
     return (
         <div className="bar-container">
-            <h2>{title}:</h2>
-            <select
-                className="selected-options"
-                value={option || ""}
-                disabled={text !== null}
-                onChange={(e) => onChangeOption(e.target.value)}
-            >
-                <option value="" disabled>
-                    Selecciona una opción
-                </option>
-                {labels.map((label, index) => {
-                    const value = values[index]
-                    return (
-                        <option key={value} value={value}>
-                            {label}
-                        </option>
-                    )
-                })}
-            </select>
+            <h2>{title} {open?option:""}:</h2>
+            <div className="custom-select">
+                <div
+                    className="selected-box"
+                    onClick={() => !text && setOpen(!open)}
+                >
+                    {
+                        option
+                            ? labels[values.indexOf(option)]
+                            : "Selecciona una opción"
+                    }
+                </div>
+                <div className={`options-container ${open ? "open" : ""}`}>
+                    {
+                        labels.map((label, index) => {
+                            const value = values[index]
+                            return (
+                                <div
+                                    key={value}
+                                    className="option-item"
+                                    onClick={() => handleSelect(value)}
+                                >
+                                    {label}
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
         </div>
     )
 }
